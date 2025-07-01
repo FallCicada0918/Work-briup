ebf3efbd75757e6eb2675bed962724002503d657
FallCicada0918 <3471437753@qq.com>
2025-07-01 15:38:16 +0800

    艺术字提交

---
+++ 1st_TestCourse/src/ArtText.java
@@ -0,0 +1,26 @@
+/*
+ * @Description: 
+ * @Author: FallCicada
+ * @Date: 2025-07-01 15:26:10
+ * @LastEditors: FallCicada
+ * @LastEditTime: 2025-07-01 15:34:31
+ * @Slogan: 無限進步
+ */
+public class ArtText {
+    public static void main(String[] args) {
+        System.out.println("███████╗ █████╗ ██╗     ██╗      ██████╗██╗ ██████╗ █████╗ ██████╗  █████╗ ");
+        System.out.println("██╔════╝██╔══██╗██║     ██║     ██╔════╝██║██╔════╝██╔══██╗██╔══██╗██╔══██╗");
+        System.out.println("█████╗  ███████║██║     ██║     ██║     ██║██║     ███████║██║  ██║███████║");
+        System.out.println("██╔══╝  ██╔══██║██║     ██║     ██║     ██║██║     ██╔══██║██║  ██║██╔══██║");
+        System.out.println("██║     ██║  ██║███████╗███████╗╚██████╗██║╚██████╗██║  ██║██████╔╝██║  ██║");
+        System.out.println("╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝");
+        System.out.println();
+        System.out.println(" █████╗ ████████╗████████╗");
+        System.out.println("██╔══██╗╚══██╔══╝╚══██╔══╝");
+        System.out.println("███████║   ██║      ██║   ");
+        System.out.println("██╔══██║   ██║      ██║   ");
+        System.out.println("██║  ██║   ██║      ██║   ");
+        System.out.println("╚═╝  ╚═╝   ╚═╝      ╚═╝   ");
+        System.out.println();
+    }
+}
